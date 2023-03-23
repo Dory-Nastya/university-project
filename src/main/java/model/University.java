971b25771a25a410cc395dc8dbd51106209b8160
Dory <ericz17@mail.ru>
2023-03-24 01:33:14 +0300

    4th lesson 4th commit annotatons added

--- src/main/java/model/University.java
+++ src/main/java/model/University.java
@@ -1,13 +1,18 @@
 package model;
 
+import com.google.gson.annotations.SerializedName;
 import enams.StudyProfile;
 
 public class University {
-
+    @SerializedName("universityId")
     private String id;
+    @SerializedName("universityName")
     private String fullName;
+    @SerializedName("universityShortName")
     private String shortName;
+    @SerializedName("foundation")
     private int yearOfFoundation;
+    @SerializedName("profile")
     private StudyProfile mainProfile;
 
     public University() {
